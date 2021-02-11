ebda8bad75ce9b1cb2b1b472a2eba577365a76b8
AmyKumar <info2amy@yahoo.com>
2021-02-10 21:52:37 -0600

    save1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AiFillLinkedin } from "react-icons/ai";
+import { FaFacebook, FaPhoneSquareAlt, FaGithub } from "react-icons/fa";
+import { GrMailOption } from "react-icons/gr";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header>
+          <a href=".logo">logo</a>
+          <a href=".about">About</a>
+          <a href=".software">Software Development</a>
+          <a href=".footer">Contact Me</a>
+        </header>
+        <footer>
+          <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+            <FaPhoneSquareAlt size="1.8em" color="black" />
+          </a>
+          <a href="mailto:someone@yoursite.com" target="_blank" rel="noreferrer">
+            <GrMailOption size="1.7em" color="black" />
+          </a>
+          <a href="https://facebook.com/" target="_blank" rel="noreferrer">
+            <AiFillLinkedin size="1.7em" color="black" />
+          </a>
+          <a href="https://facebook.com/" target="_blank" rel="noreferrer">
+            <FaGithub size="1.7em" color="black" />
+          </a>
+          <a href="https://facebook.com/" target="_blank" rel="noreferrer">
+            <FaFacebook size="1.7em" color="black" />
+          </a>
+          {/* <div className='footer-text'>Amy Kumar 2021</div> */}
+        </footer>
+      </body>
     </div>
   );
 }
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Main.css";
+
+export default function main() {
+  return (
+    <div className='layout'>BODY</div>);
+}
