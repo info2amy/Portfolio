672c05813ba1ef3ff5e5c4d9ecde9b1fa54dad1f
AmyKumar <info2amy@yahoo.com>
2021-02-11 01:05:04 -0600

    save2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import "./App.css";
 import { AiFillLinkedin } from "react-icons/ai";
 import { FaFacebook, FaPhoneSquareAlt, FaGithub } from "react-icons/fa";
 import { GrMailOption } from "react-icons/gr";
+import Project1Pic from './images/Project1Pic.png';
+import Project2Pic from './images/Project2Pic.png';
+import Project3Pic from './images/Project3Pic.png';
+import Project4Pic from './images/Project4Pic.png';
+import AboutMeArt from './images/AboutMeArt.png';
+import AboutMeBrand from './images/AboutMeBrand.png';
+import AmyTitle from './images/AmyTitle.png';
 
 function App() {
   return (
@@ -9,10 +16,38 @@ function App() {
       <body>
         <header>
           <a href=".logo">logo</a>
-          <a href=".about">About</a>
-          <a href=".software">Software Development</a>
-          <a href=".footer">Contact Me</a>
+          <a href=".about-container">About</a>
+          <a href=".projects-container">Software Development</a>
+          <a href=".connect-container">Contact Me</a>
         </header>
+
+        <container className='main-container'>
+            <img src={AmyTitle} alt="AmyTitle"/>
+        </container>
+
+        <container className='about-container'>
+          <h2>About Me</h2>
+          <section className='about-section'>
+            <img src={AboutMeArt} alt="AboutMeArt"/>
+            <img src={AboutMeBrand} alt="AboutMeBrand"/>
+          </section>
+        </container>
+
+        <container className='projects-container'>
+          <h2>Select Projects</h2>
+          <section className='projects-section'>
+            <img src={Project1Pic} alt="project1pic"/>
+            <img src={Project2Pic} alt="project2pic"/>
+            <img src={Project3Pic} alt="project3pic"/>
+            <img src={Project4Pic} alt="project4pic"/>
+          </section>
+        </container>
+
+        <container className='connect-container'>
+          <hr/>
+          <h2>Let's Connect!</h2>
+        </container>
+
         <footer>
           <a href="https://twitter.com/" target="_blank" rel="noreferrer">
             <FaPhoneSquareAlt size="1.8em" color="black" />
