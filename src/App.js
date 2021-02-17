0411cd5085e5e7055578c0e1e1d08e79da161abd
AmyKumar <info2amy@yahoo.com>
2021-02-16 21:10:02 -0600

    save8

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import AboutMeArtwork from "./images/AboutMeArtwork.png";
 import AboutMeBrand from "./images/AboutMeBrand.png";
 import AmyTitle from "./images/AmyTitle.png";
 import AmyLogo from "./images/AmyLogo.png";
+import Languages from "./images/Languages.png";
 
 function App() {
   return (
     <div className="App">
       <body>
         <header>
-          <img className="logo" src={AmyLogo} alt="logo" />
-          {/* <a href=".logo">logo</a> */}
-          <a href=".about-container">About</a>
-          <a href=".projects-container">Software Development</a>
-          <a href=".connect-container">Contact Me</a>
+          <a href="/">
+            <img className="logo" id="logo" src={AmyLogo} alt="logo" />
+          </a>
+          <a href="#about-container">About</a>
+          <a href="#projects-container">Software Development</a>
+          <a href="#connect-container">Contact Me</a>
         </header>
 
         <container className="main-container">
@@ -28,66 +30,76 @@ function App() {
         </container>
 
         <h2>About Me</h2>
-        <container className="about-container">
+        <container className="about-container" id="about-container">
           <section className="about-section">
             <img src={AboutMeArtwork} alt="AboutMeArtwork" />
-            <img src={AboutMeBrand} alt="AboutMeBrand" />
+            <img src={AboutMeBrand} alt="AboutMeBrand" /> <br/>
+            <img id='lang' src={Languages} alt="Languages" /> <br/>
           </section>
         </container>
 
         <h2>Select Projects</h2>
-        <container className="projects-container">
+        <container className="projects-container" id="projects-container">
           <section className="projects-section-1">
-            <img src={Project1Pic} alt="project1pic" />
-            <section className='text' >
-              <h4>What's for Dinner?</h4>
-            <p>My first project, after 10 days of lessons!</p></section>
-            </section> <br/>
+          <a href="https://info2amy.github.io/Whats-for-dinner/"><img src={Project1Pic} alt="project1pic" /></a>
+            <section className="text">
+              <h3>What's for Dinner?</h3>
+              <p>My first project, after 10 days of lessons!</p>
+              <p>This app was built using HTML, CSS, JavaScript and Axios along with a third-party API.  I was really pleased with making one of the containers look like a dining table, which becomes set for dinner once a main ingredient is entered! And I added a YouTube link for prep information.</p>
+            </section>
+          </section>
+          <br />
           <section className="projects-section-2">
-            <img src={Project2Pic} alt="project2pic" />
-            <section className='text' >
-              <h4>Clearlake</h4>
-            <p>My second project, after 20 days of lessons!</p></section>
+          <a href="https://clearlake-club-inc.netlify.app"><img src={Project2Pic} alt="project2pic" /></a>
+            <section className="text">
+              <h3>Clearlake</h3>
+              <p>My second project, after 20 days of lessons!</p>
+              <p>To utilize and develop my understanding of React, this app was created as a site to gather and share family pictures, memories and history.  Airtable API was utilized for data creation and storage, and also employed were React, React Router and Axios, as well as CSS styling.</p>
             </section>
+          </section>
           <section className="projects-section-3">
-            <img src={Project3Pic} alt="project3pic" />
-            <section className='text' >
-              <h4>The Wanderin' Bard</h4>
-            <p>My third project, after 30 days of lessons!</p></section>
+          <a href="https://jovial-meitner-83abc5.netlify.app/"><img src={Project3Pic} alt="project3pic" /></a>
+            <section className="text">
+              <h3>The Wanderin' Bard</h3>
+              <p>My third project, after 30 days of lessons!</p>
+              <p>The Wanderin' Bard is a webstore for unusual instruments from around the world. It was designed as a group partnership, giving experience with collaboration and Git version control.  It uses a React frontend and a backend built with Express and MongoDB.</p>
             </section>
+          </section>
           <section className="projects-section-4">
-            <img src={Project4Pic} alt="project4pic" />
-            <section className='text' >
-              <h4>The Gardener's Journal</h4>
-            <p>My fourth project, after 40 days of lessons!</p></section>
+          <a href="https://the-gardeners-journal.netlify.app/"><img src={Project4Pic} alt="project4pic" /></a>
+            <section className="text">
+              <h3>The Gardener's Journal</h3>
+              <p>My fourth project, after 40 days of lessons!</p>
+              <p>This application is designed to help gardeners with organizing and cataloging information about their favorite plants, whether in their home, their garden or in their dreams!  Built as a full-stack project with Ruby on Rails for the backend and React for the frontend, this app has full CRUD capabilities as well as user authentication.</p>
+            </section>
           </section>
         </container>
 
-        <container className="connect-container">
+        <container className="connect-container" id="connect-container">
           <hr />
           <h2>Let's Connect!</h2>
         </container>
 
         <footer>
           <a href="https://twitter.com/" target="_blank" rel="noreferrer">
-            <FaPhoneSquareAlt size="1.8em" color="black" />
+            <FaPhoneSquareAlt size="3em" color="black" />
           </a>
           <a
             href="mailto:someone@yoursite.com"
             target="_blank"
             rel="noreferrer"
           >
-            <GrMailOption size="1.7em" color="black" />
+            <GrMailOption size="3em" color="black" />
           </a>
           <a href="https://facebook.com/" target="_blank" rel="noreferrer">
-            <AiFillLinkedin size="1.7em" color="black" />
+            <AiFillLinkedin size="3em" color="black" />
           </a>
           <a href="https://facebook.com/" target="_blank" rel="noreferrer">
-            <FaGithub size="1.7em" color="black" />
+            <FaGithub size="3em" color="black" />
           </a>
-          <a href="https://facebook.com/" target="_blank" rel="noreferrer">
-            <FaFacebook size="1.7em" color="black" />
-          </a>
+          {/* <a href="https://facebook.com/" target="_blank" rel="noreferrer">
+            <FaFacebook size="3em" color="black" />
+          </a> */}
           {/* <div className='footer-text'>Amy Kumar 2021</div> */}
         </footer>
       </body>
